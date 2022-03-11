02a7f07ac4f6db508aaabebd2325e614d33a2f64
ozkancondek <ozkncndek@gmail.com>
2022-03-11 21:08:00 +0100

    C11: authentication middleware for profile page

--- routes/ProfileRouter.js
+++ routes/ProfileRouter.js
@@ -4,16 +4,18 @@ const router = express.Router();
 
 const ProfileController = require("../controllers/ProfileController");
 
+const auth = require("../middleware/authMiddleware");
+
 //base url: api/profile
 
 /**
- * @route GET /api/auth/register
+ * @route GET /api/auth/profile
  * @desc register endpoint
  * @accsess private
  */
 
-router.post("/", (req, res) => {
-  res.send("profile page");
+router.get("/", auth, (req, res) => {
+  res.send(req.decodedUser);
 });
 
 module.exports = router;
