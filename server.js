b745bd9477d4f74f1363f855233302a70ed793b2
ozkancondek <ozkncndek@gmail.com>
2022-03-12 09:10:42 +0100

    C14: Add to server.js production tools

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ app.use(express.json());
 
 app.use("/api", router);
 
+// production
+if (process.env.NODE_ENV === "production") {
+  //mw for using files in build folder on client side
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.use(express.static("client/build"));
+
 app.listen(process.env.PORT, () => {
   console.log("I am listening ");
 });
